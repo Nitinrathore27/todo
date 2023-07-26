ea3c3234c7ff34ca642fff86ad781b58ea1e3def
Nitinrathore27 <aahara.nitin.27@gmail.com>
2023-07-27 02:18:44 +0530

    first commit

---
+++ todo-list/src/NewTodoForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function NewTodoForm({onSubmit} ) {
+  const [newItem, setNewItem] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+    onSubmit(newItem);
+    setNewItem("");
+  }
+  return (
+    // <div className="toDoParent">
+       <form onSubmit={handleSubmit} className="new-item-form">
+      <label htmlFor="item">New Item</label>
+      <div className="form-row">
+        <input
+        className="formInput"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+        <button className="btn"> Add</button>
+      </div>
+      
+    </form>
+    // </div>
+   
+  );
+}
